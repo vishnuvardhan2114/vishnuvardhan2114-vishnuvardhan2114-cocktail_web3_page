8ba05f6e6c3ea2a6df9be05588e6014b6290ae40
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2025-06-22 19:36:06 +0530

    Update index.html title and favicon; add Navbar and Hero components; create constants for navigation and cocktail data.

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <Navbar />  
+      <Hero />
+      <div className="h-dvh bg-black" />
+    </main>
   );
 };
 
