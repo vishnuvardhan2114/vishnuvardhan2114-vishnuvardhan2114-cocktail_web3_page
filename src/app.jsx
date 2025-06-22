8be7f3949c376f84b82381dc0fd060282c75cfb8
vishnuvardhan2114 <vishnuvardhan14102002@gmail.com>
2025-06-22 20:28:53 +0530

    Refactor App component imports to use consistent file extensions and improve formatting

--- src/app.jsx
+++ src/app.jsx
@@ -1,29 +1,28 @@
-import React from "react";
+import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
-import gsap from "gsap";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Cocktails from "./components/Cocktails";
-import About from "./components/About";
-import Art from "./components/Art";
-import Menu from "./components/Menu";
-import Contact from "./components/Contact";
+
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
+import Art from './components/Art.jsx'
+import Menu from './components/Menu.jsx'
+import Contact from './components/Contact.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Cocktails />
-      <About />
-      <Art />
-      <Menu />
-      <Contact />
-      
-    </main>
-  );
-};
+ return (
+	<main>
+	 <Navbar />
+	 <Hero />
+	 <Cocktails />
+	 <About />
+	 <Art />
+	 <Menu />
+	 <Contact />
+	</main>
+ )
+}
 
-export default App;
+export default App
